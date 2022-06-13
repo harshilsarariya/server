783ffc89da365a328555843ad802e7c3327afc30
Harshil Sarariya <harshilsarariya@users.noreply.github.com>
2022-06-14 00:19:31 +0530

    Update | Fetch  Complaints | Search - APIs Added

--- routes/complaint.js
+++ routes/complaint.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const Complaint = require("../models/ComplaintSchema");
+var fetchuser = require("../middleware/fetchuser");
 const { body, validationResult } = require("express-validator");
+const { isValidObjectId } = require("mongoose");
 
 // ROUTE 1: Add a new Complaint using : POST "/api/complaint" ,  login required
 
@@ -70,4 +72,359 @@ router.post(
   }
 );
 
+// ROUTE 2: Update an existing Complaint using : PUT "api/complaint/updatecomplaint" ,  login required
+
+router.put(
+  "/updatecomplaint/:id",
+  fetchuser,
+  [
+    body("partyName", "Enter a valid Party name").isLength({
+      min: 3,
+    }),
+    body("address", "Enter a valid Address ").isLength({
+      min: 3,
+    }),
+    body("pincode", "Enter a valid Pincode").isLength({ min: 6 }),
+    body("mobileNo", "Enter a valid Mobile Number").isLength({ min: 8 }),
+    body("plumbingNo", "Enter a valid Plumbing Number").isLength({ min: 1 }),
+    body("brandName", "Enter a valid Brand Name").isLength({ min: 3 }),
+    body("syphoneColor", "Enter a valid Syphone Color").isLength({ min: 3 }),
+  ],
+  async (req, res) => {
+    try {
+      const {
+        partyName,
+        address,
+        pincode,
+        state,
+        city,
+        mobileNo,
+        plumbingNo,
+        brandName,
+        workDone,
+        problemSolved,
+        repeat,
+        syphoneColor,
+      } = req.body;
+
+      // const { complaintId } = req.params;
+
+      if (!isValidObjectId(req.params.id)) {
+        return res.status(401).json({ error: "Invalid Request" });
+      }
+      // Find the note to be updated and update it
+      let complaint = await Complaint.findById(req.params.id);
+      if (!complaint) {
+        return res.status(404).send("Complaint Not Found");
+      }
+
+      complaint.partyName = partyName;
+      complaint.address = address;
+      complaint.pincode = pincode;
+      complaint.state = state;
+      complaint.city = city;
+      complaint.mobileNo = mobileNo;
+      complaint.plumbingNo = plumbingNo;
+      complaint.brandName = brandName;
+      complaint.workDone = workDone;
+      complaint.problemSolved = problemSolved;
+      complaint.repeat = repeat;
+      complaint.syphoneColor = syphoneColor;
+
+      await complaint.save();
+      res.json({
+        complaint: {
+          partyName,
+          address,
+          pincode,
+          state,
+          city,
+          mobileNo,
+          plumbingNo,
+          brandName,
+          workDone,
+          problemSolved,
+          repeat,
+          syphoneColor,
+        },
+      });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal server error\n" + error.message);
+    }
+  }
+);
+
+// ROUTE 3: Get all Complaints using : GET "api/complaint/fetchallcomplaints" ,  login required
+
+router.get("/fetchallcomplaints", fetchuser, async (req, res) => {
+  try {
+    const complaints = await Complaint.find({});
+    res.json(complaints);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal server error");
+  }
+});
+
+// ROUTE 4: Delete an existing Complaint using : DELETE "api/complaint/deletecomplaint" ,  login required
+
+router.delete("/deletecomplaint/:id", fetchuser, async (req, res) => {
+  try {
+    const prmId = req.params.id;
+    if (!isValidObjectId(prmId))
+      return res.status(401).json({ error: "Invalid Request" });
+    // Find the note to be updated and update it
+    let complaint = await Complaint.findById(prmId);
+
+    if (!complaint) {
+      return res.status(404).send("Complaint Not Found!");
+    }
+
+    complaint = await Complaint.findByIdAndDelete(prmId);
+    res.json({ Success: "Note has been deleted", complaint: complaint });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal server error");
+  }
+});
+
+// ROUTE 5: Get a single Note using : GET "api/complaint/fetchcomplaint/:id" ,  login required
+
+router.get("/fetchcomplaint/:id", fetchuser, async (req, res) => {
+  try {
+    const prmId = req.params.id;
+    if (!isValidObjectId(prmId))
+      return res.status(401).json({ error: "Invalid Request" });
+
+    // Find the note to be updated and update it
+    let complaint = await Complaint.findById(prmId);
+    console.log(complaint);
+    if (!complaint) {
+      return res.status(404).send("Complaint Not Found!");
+    }
+    const {
+      partyName,
+      address,
+      pincode,
+      state,
+      city,
+      mobileNo,
+      plumbingNo,
+      brandName,
+      workDone,
+      problemSolved,
+      repeat,
+      syphoneColor,
+    } = complaint;
+
+    res.json({
+      complaint: {
+        id: complaint._id,
+        partyName,
+        address,
+        pincode,
+        state,
+        city,
+        mobileNo,
+        plumbingNo,
+        brandName,
+        workDone,
+        problemSolved,
+        repeat,
+        syphoneColor,
+      },
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal server error");
+  }
+});
+
+// ROUTE 6: Search Complaints using : GET "api/complaint/search" ,  login required
+
+router.get("/search", fetchuser, async (req, res) => {
+  try {
+    const { search } = req.query;
+    console.log(search);
+    if (!search.trim())
+      return res.status(401).json({ error: "search query is missing!" });
+    const complaints = await Complaint.find({
+      $or: [
+        { partyName: { $regex: search, $options: "i" } },
+        { address: { $regex: search, $options: "i" } },
+        { state: { $regex: search, $options: "i" } },
+        { city: { $regex: search, $options: "i" } },
+        { brandName: { $regex: search, $options: "i" } },
+        { syphoneColor: { $regex: search, $options: "i" } },
+      ],
+    });
+    res.json({
+      complaints: complaints.map((complaint) => {
+        return {
+          id: complaint._id,
+          partyName: complaint.partyName,
+          address: complaint.address,
+          pincode: complaint.pincode,
+          state: complaint.state,
+          city: complaint.city,
+          mobileNo: complaint.mobileNo,
+          plumbingNo: complaint.plumbingNo,
+          brandName: complaint.brandName,
+          workDone: complaint.workDone,
+          problemSolved: complaint.problemSolved,
+          repeat: complaint.repeat,
+          syphoneColor: complaint.syphoneColor,
+        };
+      }),
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal server error\n" + error.message);
+  }
+});
+
+// ROUTE 7: Search Complaint by status using : GET "api/complaint/searchByStatus" ,  login required
+
+router.get("/searchByOpen", fetchuser, async (req, res) => {
+  try {
+    const { search } = req.query;
+    console.log(search);
+    if (!search.trim())
+      return res.status(401).json({ error: "search query is missing!" });
+    const complaints = await Complaint.find({
+      $or: [
+        { problemSolved: { $regex: search, $options: "i" } === "No" },
+        {
+          workDone: { $regex: search, $options: "i" } === "No",
+        },
+      ],
+    });
+    res.json({
+      complaints: complaints.map((complaint) => {
+        return {
+          id: complaint._id,
+          partyName: complaint.partyName,
+          address: complaint.address,
+          pincode: complaint.pincode,
+          state: complaint.state,
+          city: complaint.city,
+          mobileNo: complaint.mobileNo,
+          plumbingNo: complaint.plumbingNo,
+          brandName: complaint.brandName,
+          workDone: complaint.workDone,
+          problemSolved: complaint.problemSolved,
+          repeat: complaint.repeat,
+          syphoneColor: complaint.syphoneColor,
+        };
+      }),
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal server error\n" + error.message);
+  }
+});
+router.get("/searchByClosed", fetchuser, async (req, res) => {
+  try {
+    const { workDone } = req.query;
+    console.log(workDone);
+    if (!workDone.trim())
+      return res.status(401).json({ error: "workDone query is missing!" });
+    const complaints = await Complaint.find({
+      workDone: { $regex: workDone, $options: "i" },
+    });
+    res.json({
+      complaints: complaints.map((complaint) => {
+        return {
+          id: complaint._id,
+          partyName: complaint.partyName,
+          address: complaint.address,
+          pincode: complaint.pincode,
+          state: complaint.state,
+          city: complaint.city,
+          mobileNo: complaint.mobileNo,
+          plumbingNo: complaint.plumbingNo,
+          brandName: complaint.brandName,
+          workDone: complaint.workDone,
+          problemSolved: complaint.problemSolved,
+          repeat: complaint.repeat,
+          syphoneColor: complaint.syphoneColor,
+        };
+      }),
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal server error\n" + error.message);
+  }
+});
+router.get("/searchByInProgress", fetchuser, async (req, res) => {
+  try {
+    const { problemSolved } = req.query;
+    console.log(problemSolved);
+    if (!problemSolved.trim())
+      return res.status(401).json({ error: "problemSolved query is missing!" });
+    const complaints = await Complaint.find({
+      problemSolved: { $regex: problemSolved, $options: "i" },
+    });
+    res.json({
+      complaints: complaints.map((complaint) => {
+        return {
+          id: complaint._id,
+          partyName: complaint.partyName,
+          address: complaint.address,
+          pincode: complaint.pincode,
+          state: complaint.state,
+          city: complaint.city,
+          mobileNo: complaint.mobileNo,
+          plumbingNo: complaint.plumbingNo,
+          brandName: complaint.brandName,
+          workDone: complaint.workDone,
+          problemSolved: complaint.problemSolved,
+          repeat: complaint.repeat,
+          syphoneColor: complaint.syphoneColor,
+        };
+      }),
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal server error\n" + error.message);
+  }
+});
+
+// Route 8: Fetch recent Complaint by repeat using : GET "api/complaint/fetchComplaints" ,  login required
+
+router.get("/fetchComplaints", fetchuser, async (req, res) => {
+  try {
+    const { pageNo = 0, limit = 5 } = req.query;
+
+    const complaints = await Complaint.find({})
+      .sort({ date: -1 })
+      .skip(parseInt(pageNo) * parseInt(limit))
+      .limit(parseInt(limit));
+
+    const complaintCount = await Complaint.countDocuments();
+
+    res.json({
+      complaints: complaints.map((complaint) => ({
+        id: complaint._id,
+        partyName: complaint.partyName,
+        address: complaint.address,
+        pincode: complaint.pincode,
+        state: complaint.state,
+        city: complaint.city,
+        mobileNo: complaint.mobileNo,
+        plumbingNo: complaint.plumbingNo,
+        brandName: complaint.brandName,
+        workDone: complaint.workDone,
+        problemSolved: complaint.problemSolved,
+        repeat: complaint.repeat,
+        syphoneColor: complaint.syphoneColor,
+      })),
+      complaintCount,
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal server error\n" + error.message);
+  }
+});
 module.exports = router;
