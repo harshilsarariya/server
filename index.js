26dd4a35ea814d97ec3406c541a7e15b387389f1
Harshil Sarariya <harshilsarariya@users.noreply.github.com>
2022-06-18 23:55:23 +0530

    for hosting changes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ connectToMongo();
 const app = express();
 const port = 5000;
 
-app.use(cors());
+app.use(cors({ origin: "http://localhost:3000" }));
 
 // add middleware for sending json
 app.use(express.json());
